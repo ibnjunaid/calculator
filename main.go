30177ef182223d11396575df4cb27bd1a747386e
Farah Nasreen <farahnasreen2000@gmail.com>
2021-01-10 20:24:45 +0530

    Implement calculate using switch

--- main.go
+++ main.go
@@ -5,7 +5,30 @@ import (
 )
 
 func main() {
-	calculateUsingIfelse()
+	calculateUsingSwitch()
+	// calculateUsingIfelse()
+
+}
+
+func calculateUsingSwitch() {
+	var a, b int
+	var op string
+	fmt.Print("enter two numbers\n")
+	fmt.Scanf("%d %s %d", &a, &op, &b)
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		fmt.Println(a / b)
+	case "%":
+		fmt.Println(a % b)
+	default:
+		fmt.Println("not found")
+	}
 }
 
 func calculateUsingIfelse() {
